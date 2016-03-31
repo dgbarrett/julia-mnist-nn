c14cbbde908c01a4a9a82bab2bd1bca3e7342ae9
Damon Barrett <barrettd@mail.uoguelph.ca>
2016-03-31 13:41:57 -0400

    Added function that generates mini batches

--- net/NeuralNetworks.jl
+++ net/NeuralNetworks.jl
@@ -1,7 +1,7 @@
 
 module SigmoidNeuralNetwork
 
-export NeuralNetwork, create_network, sigmoid
+export NeuralNetwork, create_network, gradient_descent, create_randomized_minibatches
 
 type NeuralNetwork
 	num_layers::Int64
@@ -49,6 +49,53 @@ function feed_forward( net::NeuralNetwork, activation::Array{Float64, 1} )
 		activation = sigmoid(weight*activation + bias)
 	end
 	return activation
+end
+
+function gradient_descent( net::NeuralNetwork, training_data::Array{Float64,2}, training_epochs::Int64, training_batchsize::Int64, learning_rate::Float64, test_data::Array{Float64,2}=nothing)
+	num_testitems = 0
+	if test_data != nothing 
+		num_testitems = size(test_data, 2)
+	end
+	num_trainitems = size(training_data, 2)
+
+	for i = 1:training_epochs
+		mini_batches = create_randmomized_minibatches( training_data, training_batchsize )
+	end
+
+	return
+end
+
+function create_randomized_minibatches( training_data::Array{Float64, 2}, training_batchsize::Int64 )
+	mini_batches = []
+	added_vectors = []
+	remaining = dataset_size = size(training_data, 2)
+	datavector_size = size(training_data, 1)
+
+	srand()
+
+	while remaining >= training_batchsize
+		batch = Array(Float64, datavector_size, training_batchsize)
+		for i = 1:training_batchsize
+			
+			randv = rand(1:dataset_size)
+			while randv in added_vectors
+				randv = rand(1:dataset_size)
+			end
+			push!(added_vectors, randv)
+			copyvector( batch, i, training_data, randv )
+			remaining -= 1
+
+		end
+		push!(mini_batches, batch)
+	end
+
+	return mini_batches
+end
+
+function copyvector( m1::Array{Float64, 2}, m1_index, m2::Array{Float64, 2}, m2_index )
+	for i = 1:size(m1,1)
+		m1[i, m1_index] = m2[i, m2_index]
+	end
 end
 
 function sigmoid( input::Array{Float64, 2} )
